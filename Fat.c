d9bd07703cbc12a49cb4ff283e9c16698aa50a7c
Lvi01 <lsfreitas.cic@uesc.br>
2024-12-16 20:50:03 -0300

    Primeira Alteração feita na nova branch ramo2

--- Fat.c
+++ Fat.c
@@ -5,6 +5,7 @@ int calc_fat(int);
 int main()
 {
     int numero = 5; //Modifiquei para teste
+    //Outra mudança para teste de branch
     int num_fat = calc_fat(numero);
     printf("\nO fatorial de %d eh: %d", numero , num_fat);
     return 0;
